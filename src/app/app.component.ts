5dc2c032e0936d079598b76ef054695fbc4ab76f
fabianabeda <fabiana_beda@hotmail.com>
2022-05-02 12:02:56 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+import {Usuario} from './shared/model/usuario';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  titulo = 'Social IFPB';
+
+
+  constructor() {
+  }
+
+}
